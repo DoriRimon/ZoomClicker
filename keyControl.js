c2a3b79d8bc57594127e54d13bfef87743f8c4fe
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2020-09-30 23:16:04 +0300

    Space working & Graphics are done

---
+++ keyControl.js
@@ -0,0 +1,14 @@
+document.addEventListener("keypress", event => {
+    if (event.key == " ") {
+        playButton = document.getElementsByClassName("vjs-play-control vjs-control vjs-button")[0];
+        playButton.click();
+    }
+    else if (event.key == "ArrowLeft") {
+
+    }
+    else if (event.key == "ArrowRight") {
+
+    }
+});
+
+
