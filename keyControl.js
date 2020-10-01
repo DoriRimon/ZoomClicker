dd3ba30623b366ffc5de9dd928cf71fce281079f
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2020-10-01 14:11:19 +0300

    Speed up and down

--- keyControl.js
+++ keyControl.js
@@ -1,13 +1,43 @@
+var presses = {};
+var videos = document.getElementsByTagName("video");
+var buttons = document.getElementsByTagName("button");
+const buttonClass = "vjs-play-control vjs-control vjs-button";
+
+document.addEventListener("keydown", event => {
+    presses[event.key] = (event.type == "keydown");
+    
+    if (presses["Control"] && presses["ArrowUp"]) {
+        if (videos[videos.length - 1].playbackRate < 8) {
+            videos[videos.length - 1].playbackRate += 0.25;
+            console.clear();
+            console.log("playbackRate = " + videos[videos.length - 1].playbackRate);
+        }
+    }
+
+    else if (presses["Control"] && presses["ArrowDown"]) {
+        if (videos[videos.length - 1].playbackRate >= 0.5) {
+            videos[videos.length - 1].playbackRate -= 0.25;
+            console.clear();
+            console.log("playbackRate = " + videos[videos.length - 1].playbackRate);
+        }
+    }
+});
+
+document.addEventListener("keyup", event => {
+    presses[event.key] = (event.type == "keydown");
+});
+
 document.addEventListener("keypress", event => {
     if (event.key == " ") {
-        playButton = document.getElementsByClassName("vjs-play-control vjs-control vjs-button")[0];
-        playButton.click();
-    }
-    else if (event.key == "ArrowLeft") {
-
-    }
-    else if (event.key == "ArrowRight") {
-
+        let playButton = null;
+        for (button of buttons) {
+            if (button.className.indexOf(buttonClass) != -1) {
+                playButton = button;
+                break;
+            }
+        }
+        if (playButton != null)
+            playButton.click();
     }
 });
 
