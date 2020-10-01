9b428d3e23ee842890af3746c46e9b27bfc1d252
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2020-10-01 16:15:33 +0300

    Speed forward and back

--- keyControl.js
+++ keyControl.js
@@ -6,19 +6,28 @@ const buttonClass = "vjs-play-control vjs-control vjs-button";
 document.addEventListener("keydown", event => {
     presses[event.key] = (event.type == "keydown");
     
-    if (presses["Control"] && presses["ArrowUp"]) {
-        if (videos[videos.length - 1].playbackRate < 8) {
-            videos[videos.length - 1].playbackRate += 0.25;
-            console.clear();
-            console.log("playbackRate = " + videos[videos.length - 1].playbackRate);
+    if (presses["Control"]) {
+        if (presses["ArrowUp"]) {
+            if (videos[videos.length - 1].playbackRate < 8) {
+                videos[videos.length - 1].playbackRate += 0.25;
+                console.clear();
+                console.log("playbackRate = " + videos[videos.length - 1].playbackRate);
+            }
         }
-    }
-
-    else if (presses["Control"] && presses["ArrowDown"]) {
-        if (videos[videos.length - 1].playbackRate >= 0.5) {
-            videos[videos.length - 1].playbackRate -= 0.25;
-            console.clear();
-            console.log("playbackRate = " + videos[videos.length - 1].playbackRate);
+        else if (presses["ArrowDown"]) {
+            if (videos[videos.length - 1].playbackRate >= 0.5) {
+                videos[videos.length - 1].playbackRate -= 0.25;
+                console.clear();
+                console.log("playbackRate = " + videos[videos.length - 1].playbackRate);
+            }
+        }
+        else if (presses["ArrowRight"]) {
+            if (videos[videos.length - 1].currentTime < videos[videos.length - 1].duration - 5)
+                videos[videos.length - 1].currentTime += 5;
+        }
+        else if (presses["ArrowLeft"]) {
+            if (videos[videos.length - 1].currentTime > 5)
+                videos[videos.length - 1].currentTime -= 5;
         }
     }
 });
